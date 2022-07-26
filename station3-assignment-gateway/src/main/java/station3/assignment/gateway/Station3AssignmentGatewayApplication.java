978d8543f3af192388967a897f6d92b41aca6e4f
홍준성 <ghdwnstjd13@gmail.com>
2022-07-26 19:17:58 +0900

    Spring Initializr > API Gateway

---
+++ station3-assignment-gateway/src/main/java/station3/assignment/gateway/Station3AssignmentGatewayApplication.java
@@ -0,0 +1,13 @@
+package station3.assignment.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Station3AssignmentGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Station3AssignmentGatewayApplication.class, args);
+    }
+
+}
---
+++ station3-assignment-gateway/src/test/java/station3/assignment/gateway/Station3AssignmentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package station3.assignment.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Station3AssignmentGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
