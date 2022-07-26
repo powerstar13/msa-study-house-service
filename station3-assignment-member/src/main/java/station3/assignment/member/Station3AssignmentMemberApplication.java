4b69b0160125e101d88eb53b4639fa0447e2d2eb
홍준성 <ghdwnstjd13@gmail.com>
2022-07-26 20:49:53 +0900

    Spring Initializr > Member Domain

---
+++ station3-assignment-member/src/main/java/station3/assignment/member/Station3AssignmentMemberApplication.java
@@ -0,0 +1,13 @@
+package station3.assignment.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Station3AssignmentMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Station3AssignmentMemberApplication.class, args);
+    }
+
+}
---
+++ station3-assignment-member/src/test/java/station3/assignment/member/Station3AssignmentMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package station3.assignment.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Station3AssignmentMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
