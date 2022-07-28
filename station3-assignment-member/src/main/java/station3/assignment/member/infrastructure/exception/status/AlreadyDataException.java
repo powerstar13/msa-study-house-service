d422d74e68f628642fe7c5840c18e06718b850fe
홍준성 <ghdwnstjd13@gmail.com>
2022-07-28 17:47:21 +0900

    MemberDomain > Global Exception Handling > 에러 메시지 공통 구조 반영

---
+++ station3-assignment-member/src/main/java/station3/assignment/member/infrastructure/exception/status/AlreadyDataException.java
@@ -0,0 +1,19 @@
+package station3.assignment.member.infrastructure.exception.status;
+
+import org.springframework.http.HttpStatus;
+import station3.assignment.member.infrastructure.exception.GlobalException;
+
+public class AlreadyDataException extends GlobalException {
+
+    public AlreadyDataException() {
+        super(HttpStatus.CONFLICT);
+    }
+
+    public AlreadyDataException(String reason) {
+        super(HttpStatus.CONFLICT, reason);
+    }
+
+    public AlreadyDataException(String reason, Throwable cause) {
+        super(HttpStatus.CONFLICT, reason, cause);
+    }
+}
---
+++ station3-assignment-member/src/main/java/station3/assignment/member/infrastructure/exception/status/UnauthorizedException.java
@@ -0,0 +1,19 @@
+package station3.assignment.member.infrastructure.exception.status;
+
+import org.springframework.http.HttpStatus;
+import station3.assignment.member.infrastructure.exception.GlobalException;
+
+public class UnauthorizedException extends GlobalException {
+
+    public UnauthorizedException() {
+        super(HttpStatus.UNAUTHORIZED);
+    }
+
+    public UnauthorizedException(String reason) {
+        super(HttpStatus.UNAUTHORIZED, reason);
+    }
+
+    public UnauthorizedException(String reason, Throwable cause) {
+        super(HttpStatus.UNAUTHORIZED, reason, cause);
+    }
+}
